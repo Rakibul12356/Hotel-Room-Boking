ae61bd80e00ddee048616eb7c0f1215a96a8db84
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-08-07 18:09:31 +0600

    bring room with category using query string

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,24 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types'
+import queryString from 'query-string';
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get('category');
+  const navigate = useNavigate()
+  const handleClick = () => {
+    let currentQuery = { category: label }
+    const url = queryString.stringifyUrl({
+      url: '/',
+      query: currentQuery,
+    })
+    console.log(url)
+    navigate(url) // Use replace to avoid adding to history stack
+  }
   return (
     <div
+      onClick={handleClick}
       className={`flex 
   flex-col 
   items-center 
@@ -12,7 +28,10 @@ const CategoryBox = ({ label, icon: Icon }) => {
   border-b-2
   hover:text-neutral-800
   transition
-  cursor-pointer`}
+  cursor-pointer ${
+  category=== label && 'text-neutral-800 border-b-neutral-800'
+    
+  }`}
     >
       <Icon size={26} />
       <div className='text-sm font-medium'>{label}</div>
